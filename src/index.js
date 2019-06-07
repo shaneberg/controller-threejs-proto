1c26b0806f2929e5f53904b2638f02f091865e22
Sean Haneberg <haneberg@gmail.com>
2019-06-06 23:05:12 -0700

    pull in three for index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-function component() {
-  const element = document.createElement('div');
+import * as THREE from 'three';
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "hello";
+function main() {
+  const canvas = document.getElementById('c');
+  const renderer = new THREE.WebGLRenderer({ canvas });
 
-  return element;
+  const fov = 75;
+  const aspect = 2;  // the canvas default
+  const near = 0.1;
+  const far = 5;
+  const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+
+  const scene = new THREE.Scene();
+
+  const boxWidth = 1;
+  const boxHeight = 1;
+  const boxDepth = 1;
+  const geometry = new THREE.BoxGeometry(boxWidth, boxHeight, boxDepth);
+
+  const material = new THREE.MeshBasicMaterial({ color: 0x44aa88 });
+  const cube = new THREE.Mesh(geometry, material);
+  scene.add(cube);
+  renderer.render(scene, camera);
 }
 
-document.body.appendChild(component());
+window.onload = () => {
+  main();
+}
