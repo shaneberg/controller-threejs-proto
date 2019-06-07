22e9a7626eb5d2203cf6b574a18cad74527c848a
Sean Haneberg <haneberg@gmail.com>
2019-06-06 21:53:56 -0700

    Create Development Environment (#4)
    * .gitignore for node_modules/
    
    * add webpack
    
    * add webpack-cli
    
    * add index.html and index.js
    
    * import scripts and webpack from cli-proto
    
    * move index
    
    * add html-webpack-plugin
    
    * add mini-css-extract-plugin
    
    * exclude dist/
    
    * add babel
    
    * build, run, and publish support

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+// https://www.valentinog.com/blog/webpack/
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  node: {
+    fs: "empty"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  performance: {
+    hints: false
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all'
+    }
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+};
