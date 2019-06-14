33067d0f44706cc611e1d2dccb7fa95910026012
Sean Haneberg <haneberg@gmail.com>
2019-06-13 17:46:45 -0700

    Import Tetris Engine (#11)
    * import tetris engine
    
    * Add babel presents
    
    * git movepiece working
    
    * wjhitespace

---
+++ src/engine/TetrisDefinitions.js
@@ -0,0 +1,55 @@
+class TetrisDefinitions {
+
+  static boardWidth = 10;
+  static boardHeight = 20;
+
+  static initX = 4;
+  static initY = 0;
+
+  static pieces = [
+      [
+          [1, 1],
+          [1, 0],
+          [1, 2],
+          [1, 3]
+      ], // [0] I
+      [
+          [1, 1],
+          [1, 0],
+          [1, 2],
+          [2, 2]
+      ], // [1] L
+      [
+          [1, 0],
+          [0, 0],
+          [0, 1],
+          [0, 2]
+      ], // [2] R (mirror of L)
+      [
+          [1, 0],
+          [0, 0],
+          [1, 1],
+          [2, 1]
+      ], // [3] Z
+      [
+          [0, 1],
+          [0, 0],
+          [1, 1],
+          [1, 2]
+      ], // [4] S (mirror of Z)
+      [
+          [0, 1],
+          [0, 0],
+          [1, 0],
+          [1, 1]
+      ], // [5] X
+      [
+          [1, 1],
+          [1, 0],
+          [1, 2],
+          [0, 1]
+      ] // [6] T
+  ];
+}
+
+export default TetrisDefinitions;
--- src/index.js
+++ src/index.js
@@ -1,36 +1,111 @@
 import * as THREE from 'three';
 import style from './main.css';
 
+import TetrisEngine from './engine/TetrisEngine';
+
 /**
  * Tutorial from https://threejs.org/docs/#manual/en/introduction/Drawing-lines
  */
 
 var renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-var camera = new THREE.PerspectiveCamera( 45, window.innerWidth / window.innerHeight, 1, 500 );
-camera.position.set( 0, 0, 100 );
-camera.lookAt( 0, 0, 0 );
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500);
+camera.position.set(0, 0, 100);
+camera.lookAt(0, 0, 0);
 
 var scene = new THREE.Scene();
 
 //create a blue LineBasicMaterial
-var material = new THREE.LineBasicMaterial( { color: 0x0000ff } );
+var material = new THREE.LineBasicMaterial({ color: 0x0000ff });
 
 var geometry = new THREE.Geometry();
-geometry.vertices.push(new THREE.Vector3( -10, 0, 0) );
-geometry.vertices.push(new THREE.Vector3( 0, 10, 0) );
-geometry.vertices.push(new THREE.Vector3( 10, 0, 0) );
+geometry.vertices.push(new THREE.Vector3(-10, 0, 0));
+geometry.vertices.push(new THREE.Vector3(0, 10, 0));
+geometry.vertices.push(new THREE.Vector3(10, 0, 0));
 
-var line = new THREE.Line( geometry, material );
-scene.add( line );
-renderer.render( scene, camera );
+var line = new THREE.Line(geometry, material);
+renderer.render(scene, camera);
+
+const tetrisCallback = (event, data) => {
+  console.log(event);
+  console.log(data);
+  switch (event) {
+    case 'activePieceReplaced':
+      data.blocks.forEach((block) => {
+        let rand = Math.floor(Math.random() * colors.length);
+        let mat = new THREE.LineBasicMaterial({ color: colors[rand] });
+        let renderBlock = new THREE.Line(drawBlock(block), mat);
+        scene.add(renderBlock);
+        renderBlocks.push({ renderBlock, piece: data, block });
+      });
+    case 'pieceMoved':
+      renderBlocks.forEach((renderBlock) => {
+        if (renderBlock.piece === data) {
+          const size = 1;
+          let geometry = renderBlock.renderBlock.geometry;
+          data.blocks.forEach((block) => {
+            if (renderBlock.block === block) {
+              let pos = block.pos;
+              geometry.vertices[0].x = pos.x;
+              geometry.vertices[0].y = -pos.y;
+
+              geometry.vertices[1].x = pos.x + size;
+              geometry.vertices[1].y = -pos.y;
+
+              geometry.vertices[2].x = pos.x + size;
+              geometry.vertices[2].y = -pos.y + size;
+
+              geometry.vertices[3].x = pos.x;
+              geometry.vertices[3].y = -pos.y + size;
+
+              geometry.vertices[4].x = pos.x;
+              geometry.vertices[4].y = -pos.y;
+            }
+          });
+          geometry.verticesNeedUpdate = true;
+        }
+      });
+  }
+};
+
+let engine = new TetrisEngine();
+engine.addTetrisListener(tetrisCallback);
+
+const updateTetris = () => {
+  let gameContinues = engine.step();
+  engine.step();
+  if (gameContinues) {
+    setTimeout(updateTetris, 1000);
+  }
+};
+
+let renderBlocks = [];
+
+const drawBlock = (block, mat) => {
+  let pos = block.getPos();
+
+  // TODO: Make a more sophisticated block creation policy?
+  let newBlock = new THREE.Geometry();
+  const size = 1;
+  newBlock.vertices.push(new THREE.Vector3(pos.x, -pos.y, 0));
+  newBlock.vertices.push(new THREE.Vector3(pos.x + size, -pos.y, 0));
+  newBlock.vertices.push(new THREE.Vector3(pos.x + size, -pos.y + size, 0));
+  newBlock.vertices.push(new THREE.Vector3(pos.x, -pos.y + size, 0));
+  newBlock.vertices.push(new THREE.Vector3(pos.x, -pos.y, 0));
+  return newBlock;
+}
+
+const colors = [
+  0x0000ff,
+  0x00ff00
+];
 
 var render = () => {
   requestAnimationFrame(render);
-  geometry.rotateZ(0.01);
   renderer.render(scene, camera);
 };
 
+setTimeout(updateTetris, 1000);
 render();
